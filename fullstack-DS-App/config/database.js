cb5e18f33536ddd8f70b05c22f8b833c37330a03
jevogithub <johnjevora94@gmail.com>
2023-04-30 20:57:41 -0400

    first commit

---
+++ fullstack-DS-App/config/database.js
@@ -0,0 +1,7 @@
+//config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://j_evo:vnDOpxjkpuSpJ6W2@cluster0.pctf1g0.mongodb.net/FULLSTACK_APP1?retryWrites=true&w=majority', 
+    'dbName': 'demo'
+};
+
